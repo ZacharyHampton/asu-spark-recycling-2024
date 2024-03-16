bdf7055964231483ad678bd9e713904ed5b3a924
Hololm <jholm6332@dysartstudents.org>
2024-03-16 12:46:58 -0700

    homeview wip

--- src/frontend/src/views/HomeView.vue
+++ src/frontend/src/views/HomeView.vue
@@ -2,6 +2,7 @@
   import { Button } from '../shadcn/components/ui/button/index.js'
   import { Input } from '../shadcn/components/ui/input/index.js'
   import { Search } from 'lucide-vue-next'
+  import { cn } from '@/shadcn/lib/utils.js'
   import {
   Card,
   CardContent,
@@ -13,14 +14,11 @@
 </script>
 
 <template>
-<div class="relative w-full max-w-sm items-center" style="margin:auto">
+    <div class="relative w-full max-w-sm items-center flex flex-row min-h-screen justify-center ">
     <Input id="search" type="text" placeholder="Search..." class="pl-10" />
     <span class="absolute start-0 inset-y-0 flex items-center justify-center px-2">
       <Search class="size-6 text-muted-foreground" />
     </span>
-  </div>
-  <div>
-    <Button variant="outline">Click me</Button>
   </div>
 </template>
 
