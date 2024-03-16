0f5986eaa10bae558c7e842947f339ea30acb4cb
aaron <dantesante2903@gmail.com>
2024-03-16 10:44:24 -0700

    - added nav-bar import cdn

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -3,21 +3,29 @@
   import {
     NavigationMenu,
     NavigationMenuContent,
-    NavigationMenuIndicator,
     NavigationMenuItem,
     NavigationMenuLink,
     NavigationMenuList,
     NavigationMenuTrigger,
-    NavigationMenuViewport,
-  } from '@/components/ui/navigation-menu'
+  } from '@/shadcn/components/ui/navigation-menu'
 </script>
 
 <template>
   <div>
+    <NavigationMenu>
+  <NavigationMenuList>
+    <NavigationMenuItem>
+      <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+      <NavigationMenuContent>
+        <NavigationMenuLink>Link</NavigationMenuLink>
+      </NavigationMenuContent>
+    </NavigationMenuItem>
+  </NavigationMenuList>
+</NavigationMenu>
 
   </div>
   <div>
-      <Button variant="outline">Click me</Button>
+    <Button variant="outline">Click me</Button>
   </div>
 </template>
 
